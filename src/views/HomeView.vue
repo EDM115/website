6bf83a45285cae65db688ec17e397802b369a621
EDM115 <dev@edm115.eu.org>
2023-10-19 11:44:45 +0200

    redesign@6 new prettier rules + rebuild

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+	import TheWelcome from "../components/TheWelcome.vue"
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<TheWelcome />
+	</main>
 </template>
