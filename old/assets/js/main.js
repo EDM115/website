4b736d361caf88a1fa248e57da30fa87b2301563
EDM115 <dev@edm115.eu.org>
2023-10-19 12:14:07 +0200

    closes #37

--- old/assets/js/main.js
+++ old/assets/js/main.js
@@ -133,13 +133,13 @@
 		new Waypoint({
 			element: skilsContent,
 			offset: '80%',
-			handler: function(direction) {
-				const progress = select('.progress .progress-bar', true);
+			handler(direction) {
+				let progress = select('.progress .progress-bar', true);
 				progress.forEach((el) => {
 					el.style.width = `${el.getAttribute('aria-valuenow')}%`;
 				});
-	  		}
-		})
+			}
+		});
 	}
 
 	/* Porfolio isotope and filter */
