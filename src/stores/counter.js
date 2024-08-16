2bb2a1c0faf71f286c859ebcaacce2b0ba82b7e7
Restyled.io <commits@restyled.io>
2024-08-16 10:06:04 +0000

    Restyled by prettier

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,16 +1,19 @@
-import {defineStore} from "pinia"
-import {computed, ref} from "vue"
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 
 export const useCounterStore = defineStore("counter", () => {
-	const count = ref(0)
-const doubleCount = computed(() => count.value * 2)
-/**
- * Simply increment the count by 1
- */
-        function increment() {
-  count.value++
-	}
+  const count = ref(0);
+  const doubleCount = computed(() => count.value * 2);
+  /**
+   * Simply increment the count by 1
+   */
+  function increment() {
+    count.value++;
+  }
 
-	return {
-  count, doubleCount, increment }
-})
+  return {
+    count,
+    doubleCount,
+    increment,
+  };
+});
