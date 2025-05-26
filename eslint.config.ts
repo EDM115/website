d01a0afaecf9481780ba500882b6a295b40160bb
EDM115 <dev@edm115.eu.org>
2025-05-26 15:34:44 +0200

    Renovate/eslint plugin import x 4.x lockfile (#502)
    * chore(dependencies): update dependency eslint-plugin-import-x to v4.13.3
    
    | datasource | package                | from   | to     |
    | ---------- | ---------------------- | ------ | ------ |
    | npm        | eslint-plugin-import-x | 4.13.1 | 4.13.3 |
    
    * chore: use exported flatConfigs
    
    * fix: tseslint is still complaining
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,7 +4,7 @@
 /* eslint-disable import-x/no-named-as-default-member */
 import js from "@eslint/js"
 import tsParser from "@typescript-eslint/parser"
-import eslintPluginImportX from "eslint-plugin-import-x"
+import { flatConfigs } from "eslint-plugin-import-x"
 import oxlint from "eslint-plugin-oxlint"
 import pluginVue from "eslint-plugin-vue"
 import globals from "globals"
@@ -22,8 +22,8 @@ export default [
   },
   js.configs.all,
   ...tseslint.configs.recommended,
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
+  flatConfigs.recommended,
+  flatConfigs.typescript,
   ...pluginVue.configs["flat/recommended"],
   {
     files: [ "**/*.{js,ts,vue}" ],
