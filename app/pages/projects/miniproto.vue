f21d2dd784f0ce713df07c8637a0e2c1e3261336
EDM115 <github@edm115.dev>
2026-08-15 02:21:25 +0200

    fix: correct dev branch [skip ci]

--- app/pages/projects/miniproto.vue
+++ app/pages/projects/miniproto.vue
@@ -2,5 +2,6 @@
   <HomeProjectPage
     name="EDM115/miniproto"
     glow-color="3572A5"
+    branch="codex/v1"
   />
 </template>
--- app/pages/projects/mpgram.vue
+++ app/pages/projects/mpgram.vue
@@ -2,5 +2,6 @@
   <HomeProjectPage
     name="EDM115/MPGram"
     glow-color="3572A5"
+    branch="v7-rework-part-1"
   />
 </template>
